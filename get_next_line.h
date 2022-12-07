568259d69924cf7486464931edf89dca899fcc41
Alexis Loubiere <alex.loubiere@gmail.com>
2022-12-07 07:34:25 +0100

    Added header

---
+++ get_next_line.h
@@ -0,0 +1,15 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <unistd.h>
+
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *string, int searchedChar );
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+
+#endif
+
